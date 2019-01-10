58e98bda2c204b8a8f95b0c42a285a1a25eaa228
Allen Wong <alnaym30@gmail.com>
2019-01-10 16:53:46 +0800

    Update Staff like layout

--- src/pages/ListView/components/Recommendation/index.js
+++ src/pages/ListView/components/Recommendation/index.js
@@ -3,17 +3,8 @@ import PropTypes from 'prop-types';
 import styles from './index.module.scss';
 
 function Recommendation({ inHotel = false, staffLikeCount = 0 }) {
-	let staffIcon = <span />;
-	if (staffLikeCount > 2) {
-		staffIcon =  <span className="icon icon-handy-icon-staff-fav3" />;
-	} else if (staffLikeCount > 1) {
-		staffIcon =  <span className="icon icon-handy-icon-staff-fav2" />;
-	} else if (staffLikeCount) {
-		staffIcon =  <span className="icon icon-handy-icon-staff-fav1" />;
-	}
 	return (
 		<div className={styles.Recommendation}>
-			{staffIcon}
 			{inHotel ? <span className="icon icon-handy-icon-inhotel" />: <span />}
 		</div>
 	)
--- src/pages/ListView/components/Restaurant/index.js
+++ src/pages/ListView/components/Restaurant/index.js
@@ -4,6 +4,7 @@ import classnames from 'classnames';
 import Restaurant from 'records/Restaurant';
 import { Image, Button } from 'components';
 import Recommendation from '../Recommendation';
+import StaffLike from '../StaffLike';
 import { Distance } from 'containers';
 import defaultImg from './default.svg';
 import t from 'translation';
@@ -13,34 +14,36 @@ import styles from './index.module.scss'; // Import css modules stylesheet as st
 
 function ListItem({ restaurant, ...props }) {
 	return (
-		<Button
-			{...props}
-			className={classnames(styles.ListItem, { [styles.hotel]: restaurant.get('in_hotel') })}
-		>
-			<div className={styles.imageWrapper}>
-				<Image src={restaurant.get('cover_image') || defaultImg} size={61} />
-			</div>
-			<div className={styles.contentWrapper}>
-				<span className={styles.name}>{restaurant.get('name')}</span>
-				<span className={styles.category}>{restaurant.get('category')}</span>
-				<span className={styles.district}>{restaurant.get('area')}</span>
-				{restaurant.get('budget') !== 0 ?
-					<span className={styles.price}>
-						{t('Budget: %{currency_symbol}%{budget}~', {
-							currency_symbol: '$',
-							budget: restaurant.get('budget'),
-						})}
-					</span>
-				: null}
-			</div>
-			<div className={styles.locationWrapper}>
-				<Recommendation
-					inHotel={restaurant.get('in_hotel')}
-					staffLikeCount={restaurant.get('staff_like_count')}
-				/>
-			<Distance lat={restaurant.get('lat')} lng={restaurant.get('lng')} />
-			</div>
-		</Button>
+		<div className={styles.wrapper}>
+			<Button
+				{...props}
+				className={classnames(styles.ListItem, { [styles.hotel]: restaurant.get('in_hotel') })}
+			>
+				<div className={styles.imageWrapper}>
+					<Image src={restaurant.get('cover_image') || defaultImg} size={80} />
+				</div>
+				<div className={styles.contentWrapper}>
+					<span className={styles.name}>{restaurant.get('name')}</span>
+					<span className={styles.category}>{restaurant.get('category')}</span>
+					<span className={styles.district}>{restaurant.get('area')}</span>
+					{restaurant.get('budget') !== 0 ?
+						<span className={styles.price}>
+							{t('Budget: %{currency_symbol}%{budget}~', {
+								currency_symbol: '$',
+								budget: restaurant.get('budget'),
+							})}
+						</span>
+					: null}
+				</div>
+				<div className={styles.locationWrapper}>
+					<Recommendation
+						inHotel={restaurant.get('in_hotel')}
+					/>
+					<Distance lat={restaurant.get('lat')} lng={restaurant.get('lng')} />
+				</div>
+				<StaffLike count={restaurant.get('staff_like_count')} />
+			</Button>
+		</div>
 	)
 }
 ListItem.propTypes = {
